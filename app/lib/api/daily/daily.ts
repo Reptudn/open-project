762715b3d38b05a93ddf7bd70085723b0d8aa3be
Reptudn <jonas.kauker2003@gmail.com>
2025-11-04 14:14:37 +0100

    added functionality to get daily stats from db

--- app/lib/api/daily/daily.ts
+++ app/lib/api/daily/daily.ts
@@ -1,4 +1,4 @@
-import { supabase } from "../supabase";
+import { supabase } from "@/lib/supabase";
 
 interface CalorieDayTracking {
   calories_consumed: number;
@@ -8,37 +8,50 @@ interface CalorieDayTracking {
   fats: number;
 }
 
-interface WorkoutTracking {
-  duration: number; // in minutes
+interface WorkoutSession {
+  id: string;
 }
 
 interface DayData {
+  id: number;
+  userId: string;
+  weight: number;
+  workout_session_id: WorkoutSession | null;
+  calorie_data: CalorieDayTracking[] | null;
   date: Date;
-  calorie_tracking?: CalorieDayTracking;
-  workout_tracking?: WorkoutTracking;
 }
 
-export async function getDayData(date: Date): Promise<DayData> {
-  // const { data, error } = await supabase
-  //   .from("calorie_days")
-  //   .select("*")
-  //   .eq("date", date);
-  // if (error) {
-  //   console.error("Error fetching day data:", error);
-  //   return null;
-  // }
-  // return data;
-  return {
-    date: date,
-    calorie_tracking: {
-      calories_consumed: 2000,
-      calories_burned: 500,
-      protein: 150,
-      carbs: 250,
-      fats: 70,
-    },
-    workout_tracking: {
-      duration: 60,
-    },
+export async function getDayData(date: Date): Promise<DayData | null> {
+  const { data, error } = await supabase
+    .from("daily_stats")
+    .select("*, workout_session_id(*)")
+    .eq("date", date)
+    .single();
+  if (error) {
+    console.error("Error fetching day data:", error);
+    return null;
+  }
+
+  const { data: food_data, error: err } = await supabase
+    .from("user_calorie_stats")
+    .select("*")
+    .eq("date", date);
+
+  if (err) {
+    console.error("Error fetching food data:", err);
+    return null;
+  }
+
+  const dayData: DayData = {
+    id: data.id,
+    userId: data.userId,
+    weight: data.weight,
+    workout_session_id: data.workout_session_id
+      ? null
+      : data.workout_session_id,
+    date: data.date,
+    calorie_data: err ? null : (food_data as CalorieDayTracking[]),
   };
+
+  return dayData;
 }
